f7a37b6deba4d094f54974a7d1ed1191315f816f
zpskdls <143392501+zpskdls@users.noreply.github.com>
2025-11-24 13:08:39 +0900

    Delete springboot/KGCinema directory

--- springboot/KGCinema/src/main/java/com/example/KGCinema/repository/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.KGCinema.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.KGCinema.entity.Users;
-
-public interface UsersRepository extends JpaRepository<Users, String>{
-
-}
